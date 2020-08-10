0a35afc3f0823be31f15ff2fb971b497d97f9d2b
Ghosh <Dipto.Ghosh@tmeic.in>
2020-08-10 18:31:59 +0530

    decrypt added

--- TICSUtil/functions.py
+++ TICSUtil/functions.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import configparser
-
+from cryptography.fernet import Fernet
+from getmac import get_mac_address
+import socket
 
 def log_time():
     """ Returns date time with ms. Can be used for logging messages"""
@@ -12,3 +14,17 @@ def readconfigfile(filename,section,key):
     config = configparser.ConfigParser()
     config.read(filename,  encoding='utf-8')
     return config.get(section, key)
+
+def decrypt(password):
+    encoded_key = b'U3FidkxCQ1dMSFBtcTZwU3VjVnFlaFNQRU45RHQwOGJ2azFScG0wT2ZaWT0=\n'
+    key = base_decode(encoded_key)
+    f = Fernet(key)
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    mac = get_mac_address()
+    if password.__class__ == str:
+        token = f.decrypt(password.encode('utf-8'))
+    else:
+        token = f.decrypt(password)
+    output = token.decode("utf-8").replace(ip + mac, '')
+    return output
