d0c89458e90554cdbc590cd00bb7225c199650e5
Ghosh <Dipto.Ghosh@tmeic.in>
2020-08-12 16:57:52 +0530

    class updated

--- TICSUtil/classes.py
+++ TICSUtil/classes.py
@@ -9,10 +9,17 @@ class TICSLogger:
         # Try to get configurations from os environment variables
         log_file_name = os.environ['LOG_FILENAME'] if 'LOG_FILENAME' in os.environ else 'TICSLog.log'
         directory = os.environ['LOG_FILE_DIR'] if 'LOG_FILE_DIR' in os.environ else '.\\'
-        max_filesize = os.environ['LOG_MAXSIZE'] if 'LOG_MAXSIZE' in os.environ else 5242880
-        backupCount = os.environ['LOG_BACKUP_COUNT'] if 'LOG_BACKUP_COUNT' in os.environ else 10
+        max_filesize_str = os.environ['LOG_MAXSIZE'] if 'LOG_MAXSIZE' in os.environ else 5242880
+        backupCount_str = os.environ['LOG_BACKUP_COUNT'] if 'LOG_BACKUP_COUNT' in os.environ else 10
         loggername = os.environ['LOGGER_NAME'] if 'LOGGER_NAME' in os.environ else 'TICSLog'
-
+        try:
+            max_filesize = int(max_filesize_str)
+        except:
+            max_filesize = 5242880
+        try:
+            backupCount = int(backupCount_str)
+        except:
+            backupCount = 10
         logFile = os.path.join(directory,log_file_name) if os.path.exists(directory) else log_file_name
         self.console_handler = logging.StreamHandler()
         self.console_handler.setLevel(logging.DEBUG)
