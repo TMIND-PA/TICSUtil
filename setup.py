ff0ea011f53e59f171001661ba9f5386495b6aab
dipto-ghosh30 <60124525+dipto-ghosh30@users.noreply.github.com>
2020-08-14 16:54:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='TICSUtil',
-      version='0.0.2',
+      version='0.0.3',
       description='This package is a library of small functions used in TICS development.',
       author='Sunil Goothy',
       author_email='sunil.goothy@tmeic.in',
