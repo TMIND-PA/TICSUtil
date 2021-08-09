442c1f4d22bf61fa3a136536c4124579b9fd88a2
sunilgoothy <sunil.goothy@tmeic.in>
2021-08-09 15:47:36 +0530

    license check functins

--- TICSUtil/functions.py
+++ TICSUtil/functions.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-import configparser, base64, inspect, json
+import configparser, inspect, base64, json, rsa, psutil, os
+from base64 import b64decode
 from cryptography.fernet import Fernet
 from getmac import get_mac_address
 from ipaddress import IPv4Address
@@ -51,3 +52,62 @@ def alarm_internal(rclient, alm_channel="alarm_queue", tag="internal", msg="", p
     alm_ts = str(datetime.now())
     alm_data = {"alm_tag":tag ,"value":value, "alm_name":func_name, "alm_desc":msg, "alm_priority":priority, "alm_ts":alm_ts}
     rclient.publish(alm_channel, json.dumps(alm_data))
+
+def get_mac_all(family):
+    for interface, snics in psutil.net_if_addrs().items():
+        for snic in snics:
+            if snic.family == family:
+                yield snic.address
+
+def check_license(config_file, application):
+    status = 0
+    license_path = readconfigfile(config_file,'license', 'path')
+    filename = readconfigfile(config_file,'license', 'file')
+    license_file= os.path.join(license_path, filename)
+    # Log.info(f'License file path:{license_file}')
+    if os.path.exists(license_file):
+        config = configparser.ConfigParser()
+        config.read(license_file,  encoding='utf-8')
+        if 'license' in config:
+            section = config['license']
+            if 'application' in section and 'customer' in section and \
+                'reference' in section and 'expire' in section and 'key' in section:
+                pubkey = rsa.PublicKey(7514513144282372129647063215394916456869398537697968821675421023637195249518021134485320680971729707960684956187071298549155245246725650219448549812838423, 65537)
+                addr = get_mac_all(psutil.AF_LINK)
+                for mac in addr:
+                    data =  mac + section['application'] + section['customer'] + section['reference'] + section['expire']
+                    try:
+                        rsa.verify(data.encode('utf-8'), b64decode(section['key'].encode()), pubkey)
+                    except Exception as e:
+                        msg = str(e)
+                    else:
+                        status = 1
+                        break
+                if status < 1:
+                    msg = f'This license is invalid for this machine'
+                    return status, None, None, None, msg
+                else:
+                    if datetime.datetime.strptime(section['expire'], '%Y-%m-%d').date() >= datetime.datetime.now().date():
+                        if application==section['application']:
+                            status = 1
+                            msg = f"License key Registed to {section['customer']} for Application: {section['application']} valid till: {section['expire']}"
+                            return status, section['application'], section['customer'], section['expire'], msg
+                        else:
+                            status = 101
+                            msg = f'License is not valid for the application'
+                            return status, section['application'], section['customer'], section['expire'], msg
+                    else:
+                        status = 102
+                        msg = f"License expired on {section['expire']}"
+                        return status, section['application'], section['customer'], section['expire'], msg
+            else:
+                msg = f'Invalid license file'
+                status = 103
+                return status, None, None, None, msg
+        else:
+            msg = f'Invalid license file'
+            status = 104
+            return status, None, None, None, msg
+    msg = f'License file not found'
+    status = 6
+    return status, None, None, None, msg
