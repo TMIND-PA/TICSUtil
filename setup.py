f8d02e01af439c42cff36d073d7621bd8e782f9f
sunilgoothy <sunil.goothy@tmeic.in>
2021-09-15 20:48:07 +0530

    Added color to log console. Bumped version to 1.0.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='TICSUtil',
-      version='0.0.15',
+      version='0.1.0',
       description='This package is a library of small functions used in TICS development.',
       author='Sunil Goothy',
       author_email='sunil.goothy@tmeic.in',
@@ -9,7 +9,8 @@ setup(name='TICSUtil',
         "getmac>=0.8.2",
         "cryptography>=2.9.2",
         "ipaddress>=1.0.23",
-        "rsa>=4.7.2"
+        "rsa>=4.7.2",
+        "rich>=10.9.0"
         ],
       packages=find_packages(),
     )
